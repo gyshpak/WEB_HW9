1d01f77bf3e9f506566c39d699ca8360433161f2
Gennadiy Shpak <gena_shpak@ukr.net>
2024-03-19 23:20:09 +0200

    Comm_19.03.24_23.19

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class QuotesSpider(scrapy.Spider):
         "FEED_EXPORT_ENCODING": "utf-8",
         "FEED_FORMAT": "json",
         "FEED_URI": "quotes.json",
+        "LOG_ENABLED": False,
     }
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["http://quotes.toscrape.com/"]
@@ -16,12 +17,13 @@ class QuotesSpider(scrapy.Spider):
         for quote in response.xpath("//div[@class='quote']"):
             yield {
                 "tags": quote.xpath(".//div[@class='tags']/a/text()").extract(),
-                "author": quote.xpath(".//span/small/text()").get(),
-                "quote": quote.xpath(".//span[@class='text']/text()").get(),
+                "author": quote.xpath(".//span/small/text()").get().strip(),
+                "quote": quote.xpath(".//span[@class='text']/text()").get().strip(),
             }
         next_link = response.xpath("//li[@class='next']/a/@href").get()
         if next_link:
             yield response.follow(next_link, callback=self.parse)
+
 
 class AuthorsSpider(scrapy.Spider):
     name = "authors"
@@ -29,6 +31,7 @@ class AuthorsSpider(scrapy.Spider):
         "FEED_EXPORT_ENCODING": "utf-8",
         "FEED_FORMAT": "json",
         "FEED_URI": "authors.json",
+        "LOG_ENABLED": False,
     }
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["http://quotes.toscrape.com/"]
@@ -36,24 +39,22 @@ class AuthorsSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.xpath("//div[@class='quote']"):
             author_url = quote.xpath("span/a/@href").get()
-            yield response.follow(url=response.urljoin(author_url), callback=self.parse_author)
+            yield response.follow(url=self.start_urls[0] + author_url, callback=self.parse_author)
+
+        next_link = response.xpath("//li[@class='next']/a/@href").get()
+        if next_link:
+            yield response.follow(next_link, callback=self.parse)
 
     def parse_author(self, response):
-        author_name = response.xpath("//h3/text()").get()
-        birth_date = response.xpath("//span[@class='author-born-date']/text()").get()
-        birth_place = response.xpath("//span[@class='author-born-location']/text()").get()
-        description = response.xpath("//div[@class='author-description']/text()").get()
         yield {
-            "name": author_name,
-            "birth_date": birth_date,
-            "birth_place": birth_place,
-            "description": description,
+            "fullname": response.xpath("//h3/text()").get().strip(),
+            "born_date": response.xpath("//span[@class='author-born-date']/text()").get().strip(),
+            "born_location": response.xpath("//span[@class='author-born-location']/text()").get().strip(),
+            "description": response.xpath("//div[@class='author-description']/text()").get().strip(),
         }
 
 
 if __name__ == "__main__":
-
-    # run spider
     process = CrawlerProcess()
     process.crawl(QuotesSpider)
     process.crawl(AuthorsSpider)
